945ba331e7a47376ce3f797bc594087792f5aad8
andreiito <andreortiz11@hotmail.com>
2018-02-15 08:33:49 -0600

    ADD: Archivo index.php y registro.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bienvenida</title>
+	</head>
+	<body>
+		<h1>Pagina de Inicio</h1>
+		<p>Bienvenidos a la pagina de registro UNAM</p>
+		<br/> <br/>
+		<h2>Organizaciòn</h2>
+<p>bxcz hc diuh cdjc cudcubdv kj czhvx jkcv cvjhxc </p>
+		<h2>Misiòn</h2>
+<p>hdbc ijdso ovhyi aog fkbvnsoiv d visv uijvidsv su duis gvsnd vih dvuhsd vhod shvosdvhd jhi viuvh jdnci scy csn fvius sovnv s jpcidj </p>
+		<h2>Visiòn</h2>
+<p> usha asou as hfoaolsib oaisg ac hbcyudovdiyo dsf f uadfh adiufh adfuhdfiudhaf auhpadfh uhs sufhuf s ufahif uhasfusahf saiuhf asufhs fs</p>
+		<h2>Valores</h2>
+<ul>
+	<li>aksnfi </li>
+	<li>jbiah uas</li>
+	<li>asidsa </li>
+</ul>
+<a href="registro.php">Registo</a>
+	</body>
+</html>
+	
---
+++ registro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Registro de Usuarios</title>
+	</head>
+	<body>
+		<h1>Registro al Curso</h1>
+		<p>Por favor ingrese sus datos en el siguiente formulario.</p>
+		<br/> <br/>
+		<h2>Registro</h2>
+	<form action="#" method="post">
+		<p>Nombre</p><input type="text" name="nombre"/><br/>
+		<p>Apellido Paterno</p><input type="text" name="appat"/><br/>
+		<p>Apellido Materno</p><input type="text" name="apmat"/><br/>
+		<p>Telefono</p><input type="text" name="tel"/><br/>
+		<p>Correo</p><input type="text" name="correo"/><br/>
+		<button type="submit">Enviar</button>
+	</form>
+	</body>
+</html>
+	
