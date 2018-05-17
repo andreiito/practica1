29dbc26fdf8168accfffce71fee5c32fc3ace13f
andreiito <andreortiz11@hotmail.com>
2018-05-17 08:58:14 -0500

    ADD:ajax

---
+++ get_ajax.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bienvenida</title>
+		<script src="js/jquery-3.3.1.js"></script>
+		<script src="js/ajax.js"></script>
+		<script src="js/get.js"></script>
+		<meta charset="uft-8">
+	</head>
+	<body>
+		<div id="div1"><h2>Texto que cambiarà</h2></div>
+		<button>Cargar GET</button>
+	</body>
+</html>
+	
